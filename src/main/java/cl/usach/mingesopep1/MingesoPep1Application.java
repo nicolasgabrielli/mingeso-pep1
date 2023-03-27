72fd2c6081b4916195517313184f86701e4fa9d1
Nicolás Gabrielli <nicolas.gabrielli@usach.cl>
2023-03-27 20:14:22 -0300

    Creación del Proyecto

---
+++ src/main/java/cl/usach/mingesopep1/MingesoPep1Application.java
@@ -0,0 +1,13 @@
+package cl.usach.mingesopep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingesoPep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingesoPep1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usach/mingesopep1/MingesoPep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.mingesopep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingesoPep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
